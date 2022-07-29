1d6c8fbf2c1aa75f5745e46a1258b9738be026ea
Mahdi Bagheri <qarjis8@gmail.com>
2022-07-29 17:28:05 +0430

    add Experience component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Services from './components/Services';
+import Experience from './components/Experience';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Banner />
       <Services />
+      <Experience />
     </div>
   );
 }
